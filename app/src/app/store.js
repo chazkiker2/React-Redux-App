543daa4768774baec6ad58e68c9252216ad20b46
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-11-03 21:55:56 -0500

    refactor existing project structure

--- app/src/app/store.js
+++ app/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import thunk from "redux-thunk";
 import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+	reducer: {
+		counter: counterReducer,
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
